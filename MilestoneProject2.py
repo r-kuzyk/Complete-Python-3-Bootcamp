f9d8929eb03f6c3672ed5d390bb7bb63a1450c8c
r-kuzyk <rokuzyk@gmail.com>
2018-08-07 23:01:21 +0300

    Create MilestoneProject2.py

---
+++ MilestoneProject2.py
@@ -0,0 +1,50 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
+         'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5,
+    'Six': 6,
+    'Seven': 7,
+    'Eight': 8,
+    'Nine': 9,
+    'Ten': 10,
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10,
+    'Ace': 11
+}
+playing = True
+
+
+class Card():
+    def __init__(self, suite, rank):
+        self.suite = suite
+        self.rank = rank
+
+    def __str__(self):
+        return '{} of {}'.format(self.rank, self.suite)
+
+
+class Deck():
+    def __init__(self):
+        self.deck = []
+        for suite in suits:
+            for rank in ranks:
+                self.deck.append(Card(suite, rank))
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        pass
